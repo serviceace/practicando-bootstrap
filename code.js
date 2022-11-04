2e45d73514cf1efb950a5bf5863fe84058c82cf6
Gustavo Iky Robles <the.serviceace@gmail.com>
2022-11-04 10:35:52 -0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,12 +1,36 @@
-console.log("todo conectado")
 
-const primerValor = document.querySelector('#ejemploPrueba');
-const barraProgreso = document.querySelector('#barraProgreso');
-const btnOK = document.querySelector('#btnOK');
+const primerValor = document.querySelector('#primerValor');
+const segundoValor = document.querySelector('#segundoValor');
+const tercerValor = document.querySelector('#tercerValor');
+const cuartoValor = document.querySelector('#cuartoValor');
 
-console.log(primerValor.value)
+const barraProgresoAzul = document.querySelector('#barraProgresoAzul');
+const barraProgresoAmarilla = document.querySelector('#barraProgresoAmarilla');
+const barraProgresoVerde = document.querySelector('#barraProgresoVerde');
+const barraProgresoRojo = document.querySelector('#barraProgresoRojo');
 
-btnOK.addEventListener("click", (e) => {
+const btnAzul = document.querySelector('#btnAzul');
+const btnAmarillo = document.querySelector('#btnAmarillo');
+const btnVerde = document.querySelector('#btnVerde');
+const btnRojo = document.querySelector('#btnRojo');
+
+
+btnAzul.addEventListener("click", (e) => {
     e.preventDefault();
-    barraProgreso.style.width = ejemploPrueba.value + "%";
+    barraProgresoAzul.style.width = primerValor.value + "%";
+  })
+
+btnAmarillo.addEventListener("click", (e) => {
+    e.preventDefault();
+    barraProgresoAmarilla.style.width = segundoValor.value + "%";
+  })
+
+btnVerde.addEventListener("click", (e) => {
+    e.preventDefault();
+    barraProgresoVerde.style.width = tercerValor.value + "%";
+  })
+
+btnRojo.addEventListener("click", (e) => {
+    e.preventDefault();
+    barraProgresoRojo.style.width = cuartoValor.value + "%";
   })
